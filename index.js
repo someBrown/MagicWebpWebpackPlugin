c8b73f464ab72044b6193dc6a1aa623c38fb6ddb
someBrown <raohongtai@live.com>
2024-07-10 17:48:09 +0800

    feat: refactor

--- index.js
+++ index.js
@@ -1,98 +1,19 @@
 const fs = require("fs");
 const sharp = require("sharp");
 const crypto = require("crypto");
+const { promisify } = require("util");
 
-function promisify(fn) {
-  return function () {
-    const args = Array.prototype.slice.call(arguments);
-    return new Promise((resolve, reject) => {
-      fn.apply(
-        this,
-        [].concat(args).concat([
-          (err, res) => {
-            if (err !== null) {
-              return reject(err);
-            }
-            resolve(res);
-          },
-        ])
-      );
-    });
-  };
-}
+const readFileAsync = promisify(fs.readFile);
+const writeFileAsync = promisify(fs.writeFile);
 
 const pluginName = "MagicWebpWebpackPlugin";
 
-function getContnetHash(buffer, length) {
-  // blake2s256 是 crypto 库支持的算法中最快的
-  const hash = crypto.createHash("blake2s256");
-  hash.update(buffer, "utf8");
-  const md5 = hash.digest("hex");
-  return md5.slice(0, length);
-}
-
-function addHashToPath(path, hash) {
-  const index = path.lastIndexOf(".");
-  return `${path.slice(0, index)}.${hash}${path.slice(index)}`;
-}
-
-function clearExt(path) {
-  const index = path.lastIndexOf(".");
-  return path.slice(0, index);
-}
-
-function getExt(path) {
-  const index = path.lastIndexOf(".");
-  return path.slice(index);
-}
-
-async function resolvePaths(resolver, context, request, supportExt) {
-  const requestWithoutExt = request.replace(/\.webp$/, "");
-  const customResolver = resolver.withOptions({
-    extensions: supportExt,
-  });
-  const matchedPath = await new Promise((resolve, reject) => {
-    customResolver.resolve(
-      {},
-      context,
-      requestWithoutExt,
-      {},
-      (err, result) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(result);
-        }
-      }
-    );
-  });
-  const virtualWebpPath = clearExt(matchedPath) + ".webp";
-  return {
-    matchedPath,
-    virtualWebpPath,
-  };
-}
-
-function getMathedRequest(request, matchedExt) {
-  return request.replace(/\.webp$/, matchedExt);
-}
-
-function getNextRequest(request, matchedHash) {
-  return addHashToPath(request, matchedHash);
-}
-
-const defaultOptions = {
-  supportExt: [".png", ".jpg", ".jpeg"],
-  hashLength: 6,
-  webpOptions: {
-    quality: 80,
-  },
-};
-
-module.exports = class MagicWebpWebpackPlugin {
+class MagicWebpWebpackPlugin {
   constructor(options = {}) {
     this.options = {
-      ...defaultOptions,
+      supportExt: [".png", ".jpg", ".jpeg"],
+      hashLength: 6,
+      webpOptions: { quality: 80 },
       ...options,
     };
   }
@@ -102,60 +23,163 @@ module.exports = class MagicWebpWebpackPlugin {
       (normalModuleFactory) => {
         normalModuleFactory.hooks.beforeResolve.tapPromise(
           pluginName,
-          async (data) => {
-            if (/\.webp$/.test(data.request)) {
-              const { supportExt, hashLength, webpOptions } = this.options;
-              // '@/img/xxx' 需要用到 resolver
-              const resolver = normalModuleFactory.getResolver("normal");
-              const { matchedPath, virtualWebpPath } = await resolvePaths(
-                resolver,
-                data.context,
-                data.request,
-                supportExt
-              );
-              // 不带 hash的 webp 不是本插件转换的 webp 说明本地已经有了 webp文件 直接跳过
-              const maybeExists = fs.existsSync(virtualWebpPath);
-              if (maybeExists) {
-                return;
+          async (module) => {
+            if (/\.webp$/.test(module.request)) {
+              try {
+                await this.handleBeforeResolve({
+                  module,
+                  normalModuleFactory,
+                  compiler,
+                });
+              } catch (e) {
+                console.error(e);
               }
-              const matchedImg = await promisify(fs.readFile)(matchedPath);
-              const matchedExt = getExt(matchedPath);
-              // base64 逻辑处理
-              const assetsOptions = compiler.options.module.rules.find(
-                (rule) => rule.type === "asset"
-              );
-              const dataUrlCondition =
-                assetsOptions?.parser?.dataUrlCondition?.maxSize || 8 * 1024;
-              // 如果图片小于 base64 的界限，就不转换了，但是需要把这个 webp 重定向回 supportExt
-              if (matchedImg.length < dataUrlCondition) {
-                data.request = getMathedRequest(data.request, matchedExt);
-                return;
-              }
-              const matchedHash = getContnetHash(matchedImg, hashLength);
-              const virtualHashWebpPath = addHashToPath(
-                virtualWebpPath,
-                matchedHash
-              );
-              const isExists = fs.existsSync(virtualHashWebpPath);
-              if (isExists) {
-                data.request = getNextRequest(data.request, matchedHash);
-                return;
-              }
-              const webpBuffer = await sharp(matchedImg)
-                .webp(webpOptions)
-                .toBuffer();
-
-              // 新图和旧图大小对比
-              if (matchedImg.length < webpBuffer.length) {
-                data.request = getMathedRequest(data.request, matchedExt);
-                return;
-              }
-              await promisify(fs.writeFile)(virtualHashWebpPath, webpBuffer);
-              data.request = getNextRequest(data.request, matchedHash);
             }
           }
         );
       }
     );
   }
-};
+
+  async handleBeforeResolve({ module, normalModuleFactory, compiler }) {
+    const { matchedPath, virtualWebpPath } = await this.resolvePaths({
+      normalModuleFactory,
+      module,
+    });
+
+    if (this.fileExists(virtualWebpPath) || !this.fileExists(matchedPath)) {
+      return;
+    }
+
+    const matchedImg = await readFileAsync(matchedPath);
+    const matchedExt = this.getExtension(matchedPath);
+    const matchedRequest = this.replaceExtension(module.request, matchedExt);
+
+    if (this.isBelowBase64Threshold({ buffer: matchedImg, compiler })) {
+      module.request = matchedRequest;
+      return;
+    }
+
+    const matchedHash = this.getHash(matchedImg);
+    const virtualHashWebpPath = this.addHashToPath(
+      virtualWebpPath,
+      matchedHash
+    );
+
+    const rewrittenRequest = this.addHashToPath(module.request, matchedHash);
+
+    if (this.fileExists(virtualHashWebpPath)) {
+      module.request = rewrittenRequest;
+      return;
+    }
+    const webpBuffer = await this.convertToWebp(matchedImg);
+
+    if (this.isOriginalSmaller(matchedImg, webpBuffer)) {
+      module.request = matchedRequest;
+      return;
+    }
+
+    if (this.isDevelopment()) {
+      this.cleanOldWebpFiles(matchedPath);
+    }
+
+    await writeFileAsync(virtualHashWebpPath, webpBuffer);
+    module.request = rewrittenRequest;
+  }
+
+  async resolvePaths({ normalModuleFactory, module }) {
+    const { supportExt } = this.options;
+
+    const { context: moduleContext, request } = module;
+    const resolver = normalModuleFactory.getResolver("normal");
+
+    const requestWithoutExt = this.removeExtension(request);
+
+    const customResolver = resolver.withOptions({ extensions: supportExt });
+
+    const matchedPath = await new Promise((resolve, reject) => {
+      customResolver.resolve(
+        {},
+        moduleContext,
+        requestWithoutExt,
+        {},
+        (err, result) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(result);
+          }
+        }
+      );
+    });
+
+    return {
+      matchedPath,
+      virtualWebpPath: `${this.removeExtension(matchedPath)}.webp`,
+    };
+  }
+
+  getHash(buffer) {
+    const { hashLength } = this.options;
+    const hash = crypto.createHash("blake2s256");
+    hash.update(buffer);
+    return hash.digest("hex").slice(0, hashLength);
+  }
+
+  addHashToPath(path, hash) {
+    const index = path.lastIndexOf(".");
+    return `${path.slice(0, index)}.${hash}${path.slice(index)}`;
+  }
+
+  removeExtension(path) {
+    return path.slice(0, path.lastIndexOf("."));
+  }
+
+  getExtension(path) {
+    return path.slice(path.lastIndexOf("."));
+  }
+
+  replaceExtension(request, extension) {
+    return request.replace(/\.webp$/, extension);
+  }
+
+  fileExists(path) {
+    return fs.existsSync(path);
+  }
+
+  isBelowBase64Threshold({ buffer, compiler }) {
+    const assetsOptions = compiler.options.module.rules.find(
+      (rule) => rule.type === "asset"
+    );
+    const maxSize =
+      assetsOptions?.parser?.dataUrlCondition?.maxSize || 8 * 1024;
+    return buffer.length < maxSize;
+  }
+
+  convertToWebp(buffer) {
+    const { webpOptions } = this.options;
+    return sharp(buffer).webp(webpOptions).toBuffer();
+  }
+
+  isOriginalSmaller(originalBuffer, webpBuffer) {
+    return originalBuffer.length < webpBuffer.length;
+  }
+
+  isDevelopment() {
+    return process.env.APP_ENV === "development";
+  }
+
+  cleanOldWebpFiles(matchedImgPath) {
+    const { hashLength } = this.options;
+    const dir = matchedImgPath.slice(0, matchedImgPath.lastIndexOf("/"));
+    const name = matchedImgPath.slice(matchedImgPath.lastIndexOf("/") + 1);
+    const regex = new RegExp(
+      `^${this.removeExtension(name)}\\.[a-z0-9]{${hashLength}}\\.webp$`
+    );
+    fs.readdirSync(dir)
+      .filter((file) => regex.test(file))
+      .forEach((file) => fs.unlinkSync(`${dir}/${file}`));
+  }
+}
+
+module.exports = MagicWebpWebpackPlugin;
